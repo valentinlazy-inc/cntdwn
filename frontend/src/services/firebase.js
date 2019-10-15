499c389b70678b9afbb5406ee5e9bdead0df150a
valentinlazy <valik_p@tut.by>
2019-10-16 02:05:18 +0300

    try to fix push

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -9,22 +9,12 @@ import { firebaseConfig } from '../config';
 class Firebase {
   constructor() {
     app.initializeApp(firebaseConfig);
-    // try {
-    //   this.workerPromise = navigator.serviceWorker
-    //     .register('/firebase-sw.js')
-    //     .then((registration) => {
-    //       app.messaging().useServiceWorker(registration);
-    //     });
-    // } catch (err) {
-    //   console.error(err);
-    // }
   }
   analytics() {
     app.analytics();
   }
   async askForPermissionToReceiveNotifications() {
     try {
-      // await this.workerPromise.then(() => {});
       const messaging = app.messaging();
       await messaging.requestPermission();
 
@@ -34,12 +24,10 @@ class Firebase {
     }
   }
   async getTimer(id) {
-    // await this.workerPromise.then(() => {});
     const timer = await app.database().ref('/timers/' + id).once('value');
     return timer.val();
   }
   async saveTimer(data) {
-    // await this.workerPromise.then(() => {});
     const id = app.database().ref().child('posts').push().key;
     return app.database().ref('timers/' + id).set(data).then(() => ({ ...data, id }));
   }
